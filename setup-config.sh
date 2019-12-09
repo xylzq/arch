6158d03a02fd369b79e4c2ff4278a47182777e20
xylzq <58545349+xylzq@users.noreply.github.com>
2019-12-09 16:50:29 +0800

    Update setup-config.sh

--- setup-config.sh
+++ setup-config.sh
@@ -15,29 +15,29 @@ print_title() {
 #配置系统时间,地区和语言
 configure_system(){
 	print_title "configure_system"
-	arch_chroot "ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime"
-	arch_chroot "hwclock --systohc --utc"
+	ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+	hwclock --systohc --utc
 	echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 	echo "zh_CN.UTF-8 UTF-8" >> /mnt/etc/locale.gen
-	arch_chroot "locale-gen"
+	locale-gen
 	echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 }
 
 #安装驱动程序
 configrue_drive(){
 	print_title "configrue_drive"
-        arch_chroot "pacman -S --noconfirm xorg-server xorg-twm xorg-xclock xorg-server -y"
-	arch_chroot "pacman -S --noconfirm bumblebee -y"
-        arch_chroot "systemctl enable bumblebeed"
-        arch_chroot "pacman -S --noconfirm nvidia xf86-input-synaptics -y"        
-        arch_chroot "pacman -S --noconfirm nvidia linux-lts intel-ucode linux-headers -y"
+        pacman -S --noconfirm xorg-server xorg-twm xorg-xclock xorg-server -y
+	pacman -S --noconfirm bumblebee -y
+        systemctl enable bumblebeed
+        pacman -S --noconfirm nvidia xf86-input-synaptics -y       
+        pacman -S --noconfirm nvidia linux-lts intel-ucode linux-headers -y
 }
 
 #安装网络管理程序
 configrue_networkmanager(){
        print_title "configrue_networkmanager"
-       arch_chroot "pacman -S --noconfirm iw wireless_tools wpa_supplicant dialog netctl networkmanager networkmanager-openconnect rp-pppoe network-manager-applet net-tools -y"
-       arch_chroot "systemctl enable NetworkManager.service"      
+       pacman -S --noconfirm iw wireless_tools wpa_supplicant dialog netctl networkmanager networkmanager-openconnect rp-pppoe network-manager-applet net-tools -y
+       systemctl enable NetworkManager.service      
 }
 
 
@@ -51,20 +51,20 @@ configure_hostname(){
 	else
 	cp /mnt/etc/hosts.aui /mnt/etc/hosts
 	fi
-	arch_chroot "sed -i '/127.0.0.1/s/$/ '${host_name}'/' /etc/hosts"
-	arch_chroot "sed -i '/::1/s/$/ '${host_name}'/' /etc/hosts"
-	arch_chroot "passwd"
+	sed -i '/127.0.0.1/s/$/ '${host_name}'/' /etc/hosts
+	sed -i '/::1/s/$/ '${host_name}'/' /etc/hosts
+	passwd"
   }
   
 #添加本地域名
 configure_username(){
         print_title "configure_username"
         read -p "Username [ex: archlinux]: " User
-        arch_chroot "pacman -S --noconfirm sudo zsh -y"
-        arch_chroot "useradd -m -g users -G wheel -s /bin/zsh $User"
-        arch_chroot "passwd $User"
-        arch_chroot "sed -i 's/\# \%wheel ALL=(ALL) ALL/\%wheel ALL=(ALL) ALL/g' /etc/sudoers"
-	arch_chroot "sed -i 's/\# \%wheel ALL=(ALL) NOPASSWD: ALL/\%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers"
+        pacman -S --noconfirm sudo zsh -y
+        useradd -m -g users -G wheel -s /bin/zsh $User
+        passwd $User
+        sed -i 's/\# \%wheel ALL=(ALL) ALL/\%wheel ALL=(ALL) ALL/g' /etc/sudoers
+	sed -i 's/\# \%wheel ALL=(ALL) NOPASSWD: ALL/\%wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
 	print_title "install has been.please reboot ."
 }
 
