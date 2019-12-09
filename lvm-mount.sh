6ef19745ade0181326023b235e88b80e9c46e53b
xylzq <58545349+xylzq@users.noreply.github.com>
2019-12-09 12:45:17 +0800

    Update lvm-mount.sh

--- lvm-mount.sh
+++ lvm-mount.sh
@@ -12,13 +12,25 @@ print_title() {
 	echo ""
 }
 
+#替换仓库列表
+update_mirrorlist(){
+	print_title "update_mirrorlist"
+	tmpfile=$(mktemp --suffix=-mirrorlist)	
+	url="https://www.archlinux.org/mirrorlist/?country=CN&protocol=http&protocol=https&ip_version=4"
+	curl -so ${tmpfile} ${url} 
+	sed -i 's/^#Server/Server/g' ${tmpfile}
+	mv -f ${tmpfile} /etc/pacman.d/mirrorlist;
+        pacman -Syy --noconfirm
+}
+
 #开始分区
 create_partitions(){
 	print_title "create_partitions"
 	parted -s /dev/sda mklabel msdos
 	parted -s /dev/sda mkpart primary ext4 2M 525M
 	parted -s /dev/sda mkpart primary ext4 525M 100%
-	vgcreate lv /dev/sda1 sda2
+	vgcreate lv /dev/sda1 /dev/sda2
+	lvcreate -L 525M lv -n boot
 	lvcreate -L 4G lv -n swap
 	lvcreate -l +100%FREE lv -n root
 }
@@ -28,7 +40,7 @@ format_partitions(){
         modprobe dm-mod
         vgscan
         vgchange -ay
-	mkfs.vfat -F32 /dev/sda1 
+	mkfs.vfat -F32 /dev/lv-boot 
 	mkswap /dev/mapper/lv-swap 
 	mkfs.ext4 /dev/mapper/lv-root 
 }
