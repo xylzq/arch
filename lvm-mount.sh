938e7051bf9eae24f330ea3e6e332e0fb1f69136
xylzq <58545349+xylzq@users.noreply.github.com>
2019-12-09 13:02:46 +0800

    Update lvm-mount.sh

--- lvm-mount.sh
+++ lvm-mount.sh
@@ -40,7 +40,7 @@ format_partitions(){
         modprobe dm-mod
         vgscan
         vgchange -ay
-	mkfs.vfat -F32 /dev/lv-boot 
+	mkfs.vfat -F32 /dev/mapper/lv-boot 
 	mkswap /dev/mapper/lv-swap 
 	mkfs.ext4 /dev/mapper/lv-root 
 }
